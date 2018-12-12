28df85d65749da9568e53de4049174151442e406
andrey <androyd_94@mail.ru>
2018-12-12 16:57:53 +0500

    Добавление теста ContactCreationTests

---
+++ adressbook-dev-test/adressbook-dev-test/Data/ContactData.cs
@@ -0,0 +1,38 @@
+﻿namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstName;
+        private string lastName;
+
+        public ContactData(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+    }
+}
